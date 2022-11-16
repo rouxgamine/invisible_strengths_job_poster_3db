9fcb13df589d8b92b07d6aa188d077a66484f507
Svetlana Moes <svetlanamoes@gmail.com>
2022-11-15 20:57:23 -0500

    First commit

---
+++ controllers/apiController.js
@@ -0,0 +1,19 @@
+const apiControllerCompany = {
+    index (req, res, next) {
+      res.json(res.locals.data.company)
+    },
+    show (req, res, next) {
+      res.json(res.locals.data.company)
+    },
+    create (req, res, next) {
+      res.json(res.locals.data.company)
+    },
+    update (req, res, next) {
+      res.json(res.locals.data.company)
+    },
+    destroy (req, res, next) {
+      res.json(res.locals.data.company)
+    }
+  }
+  
+  module.exports = apiControllerCompany
