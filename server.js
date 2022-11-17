241ff01e6b88b9b9ad952ee525abe17625e1eaed
Mayerlin Duluc <may_duluc@hotmail.com>
2022-11-16 19:35:03 -0500

    Fixed typo

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 // Require Modules
 const express = require('express')
-const methodOveride = require('method-override')
+const methodOverride = require('method-override')
 const cors = require('cors')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')
