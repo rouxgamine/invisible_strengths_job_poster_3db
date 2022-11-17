20c7d2a190c173aaf7bb39d7c05a0c9f8bb7740a
Antony Yu <antonyyu@gmail.com>
2022-11-16 19:01:07 -0800

    updated typo

--- models/company.js
+++ models/company.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 
 const companySchema = new mongoose.Schema({
     CompanyName: {type: String, required: true}, 
-    CompanyWebsite:{type: String, require: true},
-    AboutCompany: {type: String, require: true}, 
+    CompanyWebsite:{type: String, required: true},
+    AboutCompany: {type: String, required: true}, 
     JobPoster: [{type: mongoose.Schema.Types.ObjectId, ref: 'JobPoster'}], 
     JobPost: [{type: mongoose.Schema.Types.ObjectId, ref:'JobPost'}]
 })
