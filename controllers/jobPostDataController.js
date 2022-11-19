c061cc49d2572c9d7373dc317dc5e4cf00790d4f
Antony Yu <antonyyu@gmail.com>
2022-11-19 15:05:48 -0800

    updated JobPost create and delete routes to push/pull jobpostID from company profile

--- controllers/jobPostDataController.js
+++ controllers/jobPostDataController.js
@@ -1,4 +1,5 @@
 const JobPost = require('../models/jobpost')
+const Company = require('../models/company')
 
 const jobPostDataController = {
   // Index,
@@ -15,18 +16,30 @@ const jobPostDataController = {
     })
   },
   // Destroy
-  destroy(req, res, next) {
-    JobPost.findByIdAndDelete(req.params.id, (err, deletedJobPost) => {
-      if (err) {
-        res.status(400).send({
-          msg: err.message
-        })
-      } else {
-        res.locals.data.jobPost = deletedJobPost
-        next()
-      }
-    })
+  async destroy(req, res, next) {
+    // JobPost.findByIdAndDelete(req.params.id, (err, deletedJobPost) => {
+    //   if (err) {
+    //     res.status(400).send({
+    //       msg: err.message
+    //     })
+    //   } else {
+    //     res.locals.data.jobPost = deletedJobPost
+    //     next()
+    //   }
+    // })
+    try {
+      const deletedJobPost = await JobPost.findByIdAndDelete(req.params.id)
+      await Company.findByIdAndUpdate(deletedJobPost.company, {
+        $pull: {
+          JobPost: req.params.id
+        }
+      })
+      res.status(200).json(deletedJobPost)
+    } catch (error) {
+      console.log(error)
+    }
   },
+
   // Update
   update(req, res, next) {
     JobPost.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, updatedJobPost) => {
@@ -40,33 +53,45 @@ const jobPostDataController = {
       }
     })
   },
-  // Create
-  create(req, res, next) {
-    JobPost.create(req.body, (err, createdJobPost) => {
-      if (err) {
-        res.status(400).send({
-          msg: err.message
-        })
-      } else {
-        res.locals.data.jobPost = createdJobPost
-        next()
+    // Create
+    async create(req, res, next) {
+  // JobPost.create(req.body, (err, createdJobPost) => {
+  //   if (err) {
+  //     res.status(400).send({
+  //       msg: err.message
+  //     })
+  //   } else {
+  //     res.locals.data.jobPost = createdJobPost
+  //     next()
+  //   }
+  // })
+  try {
+    const newJobPost = await JobPost.create(req.body)
+    await Company.findByIdAndUpdate(newJobPost.company, {
+      $push: {
+        JobPost: newJobPost._id
       }
     })
-  },
-  // Show
-  show(req, res, next) {
-    JobPost.findById(req.params.id, (err, foundJobPost) => {
-        if (err) {
-          res.status(404).send({
-            msg: err.message,
-            output: 'Could not find a jobPost with that ID'
-          })
-        } else {
-          res.locals.data.jobPost = foundJobPost
-          next()
-        }
+    res.status(200).json(newJobPost)
+  } catch (error) {
+    console.log(error)
+  }
+},
+
+// Show
+show(req, res, next) {
+  JobPost.findById(req.params.id, (err, foundJobPost) => {
+    if (err) {
+      res.status(404).send({
+        msg: err.message,
+        output: 'Could not find a jobPost with that ID'
       })
+    } else {
+      res.locals.data.jobPost = foundJobPost
+      next()
     }
+  })
+}
 }
 
 module.exports = jobPostDataController
--- models/jobPoster.js
+++ models/jobPoster.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose')
 
 const jobPosterSchema = new mongoose.Schema({
-    User_ID: {type: mongoose.Schema.Types.ObjectId, ref:'User'}, 
+    User_ID: {type: String, required: true}, 
+    Password:{type: String, required: true},
     JobPosterTitle: {type: String, required: true}, 
     JobPost: [{type: mongoose.Schema.Types.ObjectId, ref: 'JobPost'}], 
-    Company: [{type: mongoose.Schema.Types.ObjectId, ref:'Company'}],
+    Company: {type: mongoose.Schema.Types.ObjectId, ref:'Company'}
 })
 
 const JobPoster = mongoose.model('JobPoster', jobPosterSchema)
